3b101ede9d4deb4e6ccf897ef5dca7431a030a61
Leandro Gustavo Neis <leandro.neis@edu.univali.br>
2021-09-13 21:52:43 -0300

    Enviando o projeto ao repositório

---
+++ CrudAula/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace CrudAula
+{
+    partial class Locadora
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNome = new System.Windows.Forms.Label();
+            this.lbCategoria = new System.Windows.Forms.Label();
+            this.lbDescricao = new System.Windows.Forms.Label();
+            this.lbAno = new System.Windows.Forms.Label();
+            this.btLimpar = new System.Windows.Forms.Button();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.tbCategoria = new System.Windows.Forms.TextBox();
+            this.tbDescricao = new System.Windows.Forms.TextBox();
+            this.tbAno = new System.Windows.Forms.TextBox();
+            this.dgLocadora = new System.Windows.Forms.DataGridView();
+            this.codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.categoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ano = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgLocadora)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbNome
+            // 
+            this.lbNome.AutoSize = true;
+            this.lbNome.Location = new System.Drawing.Point(12, 27);
+            this.lbNome.Name = "lbNome";
+            this.lbNome.Size = new System.Drawing.Size(38, 13);
+            this.lbNome.TabIndex = 0;
+            this.lbNome.Text = "Nome:";
+            // 
+            // lbCategoria
+            // 
+            this.lbCategoria.AutoSize = true;
+            this.lbCategoria.Location = new System.Drawing.Point(173, 27);
+            this.lbCategoria.Name = "lbCategoria";
+            this.lbCategoria.Size = new System.Drawing.Size(55, 13);
+            this.lbCategoria.TabIndex = 1;
+            this.lbCategoria.Text = "Categoria:";
+            // 
+            // lbDescricao
+            // 
+            this.lbDescricao.AutoSize = true;
+            this.lbDescricao.Location = new System.Drawing.Point(337, 27);
+            this.lbDescricao.Name = "lbDescricao";
+            this.lbDescricao.Size = new System.Drawing.Size(58, 13);
+            this.lbDescricao.TabIndex = 2;
+            this.lbDescricao.Text = "Descricao:";
+            // 
+            // lbAno
+            // 
+            this.lbAno.AutoSize = true;
+            this.lbAno.Location = new System.Drawing.Point(627, 27);
+            this.lbAno.Name = "lbAno";
+            this.lbAno.Size = new System.Drawing.Size(29, 13);
+            this.lbAno.TabIndex = 3;
+            this.lbAno.Text = "Ano:";
+            // 
+            // btLimpar
+            // 
+            this.btLimpar.Location = new System.Drawing.Point(189, 97);
+            this.btLimpar.Name = "btLimpar";
+            this.btLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btLimpar.TabIndex = 4;
+            this.btLimpar.Text = "Limpar";
+            this.btLimpar.UseVisualStyleBackColor = true;
+            this.btLimpar.Click += new System.EventHandler(this.btLimpar_Click);
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(108, 97);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(75, 23);
+            this.btnInserir.TabIndex = 5;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // tbNome
+            // 
+            this.tbNome.Location = new System.Drawing.Point(12, 58);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(158, 20);
+            this.tbNome.TabIndex = 8;
+            // 
+            // tbCategoria
+            // 
+            this.tbCategoria.Location = new System.Drawing.Point(176, 58);
+            this.tbCategoria.Name = "tbCategoria";
+            this.tbCategoria.Size = new System.Drawing.Size(158, 20);
+            this.tbCategoria.TabIndex = 9;
+            // 
+            // tbDescricao
+            // 
+            this.tbDescricao.Location = new System.Drawing.Point(340, 58);
+            this.tbDescricao.Name = "tbDescricao";
+            this.tbDescricao.Size = new System.Drawing.Size(284, 20);
+            this.tbDescricao.TabIndex = 10;
+            // 
+            // tbAno
+            // 
+            this.tbAno.Location = new System.Drawing.Point(630, 58);
+            this.tbAno.Name = "tbAno";
+            this.tbAno.Size = new System.Drawing.Size(158, 20);
+            this.tbAno.TabIndex = 11;
+            // 
+            // dgLocadora
+            // 
+            this.dgLocadora.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgLocadora.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.codigo,
+            this.nome,
+            this.categoria,
+            this.descricao,
+            this.ano});
+            this.dgLocadora.Location = new System.Drawing.Point(12, 150);
+            this.dgLocadora.Name = "dgLocadora";
+            this.dgLocadora.Size = new System.Drawing.Size(777, 289);
+            this.dgLocadora.TabIndex = 12;
+            // 
+            // codigo
+            // 
+            this.codigo.HeaderText = "ID";
+            this.codigo.Name = "codigo";
+            this.codigo.Width = 30;
+            // 
+            // nome
+            // 
+            this.nome.HeaderText = "Nome";
+            this.nome.Name = "nome";
+            this.nome.Width = 170;
+            // 
+            // categoria
+            // 
+            this.categoria.HeaderText = "Categoria";
+            this.categoria.Name = "categoria";
+            this.categoria.Width = 150;
+            // 
+            // descricao
+            // 
+            this.descricao.HeaderText = "Descricao";
+            this.descricao.Name = "descricao";
+            this.descricao.Width = 300;
+            // 
+            // ano
+            // 
+            this.ano.HeaderText = "Ano";
+            this.ano.Name = "ano";
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(272, 97);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 13;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(353, 97);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 14;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // Locadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.dgLocadora);
+            this.Controls.Add(this.tbAno);
+            this.Controls.Add(this.tbDescricao);
+            this.Controls.Add(this.tbCategoria);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.btLimpar);
+            this.Controls.Add(this.lbAno);
+            this.Controls.Add(this.lbDescricao);
+            this.Controls.Add(this.lbCategoria);
+            this.Controls.Add(this.lbNome);
+            this.Name = "Locadora";
+            this.Text = "Locadora";
+            this.Load += new System.EventHandler(this.Locadora_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgLocadora)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbNome;
+        private System.Windows.Forms.Label lbCategoria;
+        private System.Windows.Forms.Label lbDescricao;
+        private System.Windows.Forms.Label lbAno;
+        private System.Windows.Forms.Button btLimpar;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.TextBox tbCategoria;
+        private System.Windows.Forms.TextBox tbDescricao;
+        private System.Windows.Forms.TextBox tbAno;
+        private System.Windows.Forms.DataGridView dgLocadora;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn categoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descricao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ano;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnExcluir;
+    }
+}
+
---
+++ CrudAula/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace CrudAula
+{
+    public partial class Locadora : Form
+    {
+        public Locadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btLimpar_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+        }
+
+        private void limparCampos()
+        {
+            tbNome.Clear();
+            tbCategoria.Clear();
+            tbDescricao.Clear();
+            tbAno.Clear();
+
+        }
+
+        private void atualizarGrid() {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexaoBD = new MySqlConnection(conexaoBD.ToString());
+            try {
+                realizaConexaoBD.Open();
+                MySqlCommand comandoMysql = realizaConexaoBD.CreateCommand();
+                comandoMysql.CommandText = "SELECT * FROM LOCADORA WHERE ativoFilme = 1";
+                MySqlDataReader reader = comandoMysql.ExecuteReader();
+                dgLocadora.Rows.Clear();
+
+                while (reader.Read()) 
+                {
+                    DataGridViewRow row = (DataGridViewRow)dgLocadora.Rows[0].Clone();
+                    row.Cells[0].Value = reader.GetInt32(0);//Id
+                    row.Cells[1].Value = reader.GetString(1);//Nome do Filme
+                    row.Cells[2].Value = reader.GetString(2);//Descrição
+                    row.Cells[3].Value = reader.GetString(3);//Categoria
+                    row.Cells[4].Value = reader.GetString(4);//Ano
+                    dgLocadora.Rows.Add(row);
+                }
+                realizaConexaoBD.Close();
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Can not open Connection!");
+            }
+        }
+
+        private MySqlConnectionStringBuilder conexaoBanco() 
+        {
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "127.0.1.1";
+            conexaoBD.Database = "locadora";
+            conexaoBD.UserID = "desenvolvimento";
+            conexaoBD.Password = "$8h&1a]PKh.J";
+            return conexaoBD;
+        }
+
+        private void Locadora_Load(object sender, EventArgs e)
+        {
+            atualizarGrid();
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexaoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexaoBD.Open();
+                MySqlCommand comandoMysql = realizaConexaoBD.CreateCommand();
+
+                comandoMysql.CommandText = "INSERT INTO locadora(nomeFilme, descricaoFilme, categoriaFilme, anoFilme) " +
+                    "VALUES('"+tbNome.Text+"', '"+tbDescricao.Text+"', '"+tbCategoria.Text+"','"+tbAno.Text+"')";
+                comandoMysql.ExecuteReader();
+                realizaConexaoBD.Close();
+                MessageBox.Show("Inserido com sucesso!");
+                atualizarGrid();
+                limparCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open Connection!");
+            }
+        }
+    }
+}
